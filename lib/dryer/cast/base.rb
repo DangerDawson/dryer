1c265b5a35656fb2a3886e0b727eecb4d8ba15d9
Layon <layon1993@gmail.com>
2016-05-20 18:17:01 +0100

    add cast_private

--- lib/dryer/cast/base.rb
+++ lib/dryer/cast/base.rb
@@ -34,6 +34,26 @@ module Dryer
             end
           end
         end
+
+        klass.define_singleton_method :cast_private do |*macro_args, &_macro_block|
+          name = macro_args.shift
+          options = macro_args.shift || {}
+          explicit_klass = options[:class_name]
+
+          define_method(name) do |*args, &block|
+            implicit_klass = [local_namespace, name.to_s.classify].join("::")
+            delegate_klass = explicit_klass ? explicit_klass : implicit_klass
+            delegate_instance = delegate_klass.constantize.new(sender: self)
+            if delegate_instance.method(:call).arity.zero?
+              delegate_instance.call(&block)
+            else
+              delegate_instance.call(*args, &block)
+            end
+          end
+          __send__(:private, name)
+        end
+
+
       end
 
     end
