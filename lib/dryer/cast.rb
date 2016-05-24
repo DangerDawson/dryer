a2df119b17981964d2c438b5f379bdf7d4ed5f29
David Dawson <david@wooolandthegang.com>
2016-05-24 20:15:32 +0100

    Reworked the constructor

--- lib/dryer/cast.rb
+++ lib/dryer/cast.rb
@@ -3,5 +3,12 @@ require "dryer/cast/target"
 
 module Dryer
   module Cast
+    def self.base
+      ::Dryer::Cast::Base.new
+    end
+
+    def self.target(*args)
+      ::Dryer::Cast::Target.new(args)
+    end
   end
 end
