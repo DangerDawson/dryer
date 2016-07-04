31813bff0c04b2d104fb0461d490e4f0f9fef26e
David Dawson <david@wooolandthegang.com>
2016-07-04 07:23:56 +0100

    Update the memory leak test

--- mem_leak.rb
+++ mem_leak.rb
@@ -21,9 +21,27 @@ class Caster
   end
 end
 
+class Base
+  include ::Dryer::Construct
+  construct(:one, two: 2)
+end
+
+class Construct < Base
+  construct(:three, four: 4)
+end
+
+class Construct2 < Base
+  construct(:five, six: 6)
+end
+
 1000.times do
   Caster.new.cast_a
+  Construct.new(one: 1, three: 3) 
+  Construct2.new(one: 1, five: 5) 
 end
 
 GC.start
 puts "Leaked Caster Objects: #{ObjectSpace.each_object(Caster).count}"
+puts "Leaked Constructer Base Objects: #{ObjectSpace.each_object(Base).count}"
+puts "Leaked Constructer Construct Objects: #{ObjectSpace.each_object(Construct).count}"
+puts "Leaked Constructer Construct2 Objects: #{ObjectSpace.each_object(Construct2).count}"
