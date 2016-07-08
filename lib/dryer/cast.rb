9811eb9f78797889440620ecb7ca6e5492155fe4
David Dawson <david@wooolandthegang.com>
2016-07-08 09:32:49 +0100

    Steal constantize from active support

--- lib/dryer/cast.rb
+++ lib/dryer/cast.rb
@@ -42,6 +42,38 @@ module Dryer
 
       private
 
+      # Stolen from active suport
+      def constantize(camel_cased_word)
+        names = camel_cased_word.split("::".freeze)
+
+        # Trigger a built-in NameError exception including the ill-formed constant in the message.
+        Object.const_get(camel_cased_word) if names.empty?
+
+        # Remove the first blank element in case of '::ClassName' notation.
+        names.shift if names.size > 1 && names.first.empty?
+
+        names.inject(Object) do |constant, name|
+          if constant == Object
+            constant.const_get(name)
+          else
+            candidate = constant.const_get(name)
+            next candidate if constant.const_defined?(name, false)
+            next candidate unless Object.const_defined?(name)
+
+            # Go down the ancestors to check if it is owned directly. The check
+            # stops when we reach Object or the end of ancestors tree.
+            constant = constant.ancestors.inject do |const, ancestor|
+              break const    if ancestor == Object
+              break ancestor if ancestor.const_defined?(name, false)
+              const
+            end
+
+            # owner is in Object, so raise
+            constant.const_get(name, false)
+          end
+        end
+      end
+
       def define_cast_group_singleton(klass)
         local_klass = klass
         klass.define_singleton_method :cast_group do |args = {}, &block|
@@ -112,7 +144,8 @@ module Dryer
         constructor_params = parse_args(caster, constructor_args)
 
         # Ensure that we do not send over an empty {} if no args are specified
-        target_klass = Kernel.const_get(target_klass)
+        target_klass = constantize(target_klass)
+
         target_instance = if constructor_params.empty?
                             target_klass.new
                           else
@@ -148,7 +181,7 @@ module Dryer
                     args.pop.merge(merge_args)
                   else
                     merge_args
-                  end
+          end
         end
         args
       end
